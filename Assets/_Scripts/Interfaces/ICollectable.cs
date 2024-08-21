6ae5226945959690a064b655e39e98effe48879d
Carterman555 <25chart555@gmail.com>
2024-08-21 09:41:14 -0600

    Stat modifier logic, start enemy behavior

---
+++ Assets/_Scripts/Interfaces/ICollectable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICollectable {
+
+    public string GetName();
+    public string GetDescription();
+
+    public Rarity GetRarity();
+
+    public int GetCost();
+
+    public Sprite GetSprite();
+}
+
+public enum Rarity {
+    Common = 0,
+    Uncommon = 1,
+    Rare = 2,
+    Epic = 3,
+    Mythic = 4
+}
---
+++ Assets/_Scripts/Managers/StatsManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsManager : StaticInstance<StatsManager> {
+
+    [SerializeField] private ScriptablePlayer scriptablePlayer;
+    private PlayerStats playerStats;
+
+    private PlayerStatsModifier allPlayerStatsModifiers;
+
+    protected override void Awake() {
+        base.Awake();
+
+        allPlayerStatsModifiers = PlayerStatsModifier.Zero;
+    }
+
+    public void AddPlayerStatsModifier(PlayerStatsModifier modifier) {
+        allPlayerStatsModifiers += modifier;
+        UpdatePlayerStats();
+    }
+
+    public void RemovePlayerStatsModifier(PlayerStatsModifier modifier) {
+        allPlayerStatsModifiers += modifier;
+        UpdatePlayerStats();
+    }
+
+    private void UpdatePlayerStats() {
+        playerStats = scriptablePlayer.Stats;
+        playerStats.ApplyModifier(allPlayerStatsModifiers); 
+    }
+
+    public PlayerStats GetPlayerStats() {
+        return playerStats;
+    }
+}

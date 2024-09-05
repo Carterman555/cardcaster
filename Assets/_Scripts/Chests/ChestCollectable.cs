78a63aa4af4751f68bf1b472c87363840b1a4a3a
Carterman555 <25chart555@gmail.com>
2024-09-05 11:40:52 -0600

    Card chest

---
+++ Assets/_Scripts/Chests/ChestCollectable.cs
@@ -0,0 +1,95 @@
+using DG.Tweening;
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChestCollectable : MonoBehaviour {
+
+    [SerializeField] private InputActionReference selectAction;
+
+    [SerializeField] private Vector2 positionOffset;
+
+    private CardChest chest;
+    private ICollectable collectable;
+    private int collectableIndex;
+
+    private bool availableToChoose;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Setup(CardChest chest, ICollectable collectable, int collectableIndex) {
+        this.chest = chest;
+        this.collectable = collectable;
+        this.collectableIndex = collectableIndex;
+
+        spriteRenderer.sprite = collectable.GetSprite();
+
+        transform.position = chest.transform.position;
+        transform.localScale = Vector2.zero;
+
+        float duration = 0.3f;
+        transform.DOLocalMove(positionOffset, duration).SetEase(Ease.OutSine);
+        transform.DOScale(Vector2.one, duration).SetEase(Ease.OutSine).OnComplete(() => {
+            availableToChoose = true;
+        });
+    }
+
+    private void Update() {
+
+        
+
+       
+    }
+
+    private void OnMouseEnter() {
+        // shine or outline - TODO
+    }
+
+    private void OnMouseOver() {
+
+        if (!availableToChoose) {
+            return;
+        }
+
+        //if (selectAction.action.triggered) {
+        if (Input.GetMouseButtonDown(0)) {
+            GoToPlayer();
+            StartCoroutine(chest.SelectCard(collectableIndex));
+        }
+    }
+
+    private void OnMouseExit() {
+        // disable shine or outline - TODO
+    }
+
+    public void GoToPlayer() {
+
+        // so it doesn't disappear when the chest does
+        transform.SetParent(Containers.Instance.Effects);
+
+        float duration = 0.5f;
+        transform.DOMove(PlayerMovement.Instance.transform.position, duration).SetEase(Ease.InSine).OnComplete(() => {
+            transform.DOScale(Vector2.zero, duration).SetEase(Ease.InSine).OnComplete(() => {
+                GainCollectable();
+            });
+        });
+    }
+
+    private void GainCollectable() {
+        if (collectable is ScriptableCardBase scriptableCard) {
+            DeckManager.Instance.GainCard(scriptableCard);
+        }
+    }
+
+    public void ReturnToChest(float duration) {
+        availableToChoose = false;
+
+        transform.DOLocalMove(Vector2.zero, duration).SetEase(Ease.InSine);
+        transform.DOScale(Vector2.zero, duration).SetEase(Ease.InSine);
+    }
+}

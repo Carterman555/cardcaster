d923a8117b0e84193e0e91947b4e768ed032be31
Carterman555 <25chart555@gmail.com>
2024-09-27 09:38:43 -0600

    Create blue rooms and work on visuals

---
+++ Assets/_Scripts/UI/MaterialOnHover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MaterialOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    [SerializeField] private Image image;
+    [SerializeField] private Material hoverMaterial;
+
+    private Material originalMaterial;
+
+    private void Awake() {
+        originalMaterial = image.material;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        image.material = hoverMaterial;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        image.material = originalMaterial;
+    }
+}

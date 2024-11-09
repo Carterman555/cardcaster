e9bd8e6ca8ac16d5960b18233f5a1baabf1c37ec
Carterman555 <25chart555@gmail.com>
2024-11-09 10:50:01 -0700

    Create room collider creator, update ui

--- Assets/_Scripts/RoomCreator/RoomCreatorWindow.cs
+++ Assets/_Scripts/RoomCreator/RoomCreatorWindow.cs
@@ -7,13 +7,15 @@ using UnityEngine.Tilemaps;
 
 public class RoomCreatorWindow : EditorWindow {
 
-    private RoomTilemapCreator roomTilemapCreator;
-
     private Tilemap groundTilemap;
     private Tilemap topWallsTilemap;
     private Tilemap botWallsTilemap;
 
+    private RoomTilemapCreator roomTilemapCreator;
     private string tileSetName;
+
+    private PolygonCollider2D roomCollider;
+    private PolygonCollider2D camConfinerCollider;
 
     [MenuItem("Tools/Room Creator")]
     public static void ShowWindow() {
@@ -26,20 +28,23 @@ public class RoomCreatorWindow : EditorWindow {
 
     private void OnGUI() {
 
-        roomTilemapCreator = EditorGUILayout.ObjectField("Room Tilemap Creator", roomTilemapCreator, typeof(RoomTilemapCreator), true) as RoomTilemapCreator;
-
         groundTilemap = EditorGUILayout.ObjectField("Ground Tilemap", groundTilemap, typeof(Tilemap), true) as Tilemap;
         topWallsTilemap = EditorGUILayout.ObjectField("Top Walls Tilemap", topWallsTilemap, typeof(Tilemap), true) as Tilemap;
         botWallsTilemap = EditorGUILayout.ObjectField("Bottom Walls Tilemap", botWallsTilemap, typeof(Tilemap), true) as Tilemap;
 
+        GUILayout.Space(5);
+        var headerRect = GUILayoutUtility.GetRect(0, height: 30, GUILayout.ExpandWidth(true));
+        EditorGUI.LabelField(headerRect, "Tilemap Creator");
+        GUILayout.Space(5);
+
+        roomTilemapCreator = EditorGUILayout.ObjectField("Room Tilemap Creator", roomTilemapCreator, typeof(RoomTilemapCreator), true) as RoomTilemapCreator;
         tileSetName = EditorGUILayout.TextField("Tile Set Name", tileSetName);
 
         if (GUILayout.Button("Create Wall Tiles")) {
             roomTilemapCreator.CreateRoomTiles(tileSetName, groundTilemap, topWallsTilemap, botWallsTilemap);
-
-            RoomColliderMatcher roomColliderMatcher = new();
-            roomColliderMatcher.SetupColliders();
         }
+
+        GUILayout.Space(5);
 
         if (GUILayout.Button("Clear Ground Tilemap")) {
             ClearTilemap(groundTilemap);
@@ -49,6 +54,22 @@ public class RoomCreatorWindow : EditorWindow {
             ClearTilemap(topWallsTilemap);
             ClearTilemap(botWallsTilemap);
         }
+
+        GUILayout.Space(5);
+        headerRect = GUILayoutUtility.GetRect(0, height: 30, GUILayout.ExpandWidth(true));
+        EditorGUI.LabelField(headerRect, "Collider Setup");
+        GUILayout.Space(5);
+
+        roomCollider = EditorGUILayout.ObjectField("Room Collider", roomCollider, typeof(PolygonCollider2D), true) as PolygonCollider2D;
+        camConfinerCollider = EditorGUILayout.ObjectField("Camera Confiner Collider", camConfinerCollider, typeof(PolygonCollider2D), true) as PolygonCollider2D;
+
+        if (GUILayout.Button("Setup Polygon Colliders")) {
+            RoomColliderMatcher roomColliderMatcher = new(roomCollider, camConfinerCollider, topWallsTilemap, botWallsTilemap);
+            roomColliderMatcher.SetupRoomCollider();
+            roomColliderMatcher.SetupCamConfinerCollider();
+        }
+
+        
     }
 
     private void ClearTilemap(Tilemap tilemap) {

5d3bcdc64f0a5c8d64f2d664c2329efa3ca90236
Carterman555 <25chart555@gmail.com>
2024-12-09 09:56:03 -0700

    Creator can create minimap icon sprites

---
+++ Assets/_Scripts/RoomCreator/ScriptableRooms.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableRooms", menuName = "RoomCreator/Rooms")]
+public class ScriptableRooms : ScriptableObject {
+    public Room[] Rooms;
+}

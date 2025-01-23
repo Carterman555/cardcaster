0746563a225338708c11eee153c7a1628b8c12fb
Carterman555 <25chart555@gmail.com>
2025-01-23 09:09:42 -0700

    Improve card names and descriptions, start reward map icons

--- Assets/_Scripts/RoomGeneration/Room.cs
+++ Assets/_Scripts/RoomGeneration/Room.cs
@@ -129,7 +129,7 @@ public class Room : MonoBehaviour {
     // spawn the map icon as a child of LevelMapIcons so LevelMapIcons can create a unified outline around all the
     // rooms and hallways
     private void SetupMapIcon() {
-        mapIcon = mapIconToSpawn.Spawn(mapIconToSpawn.transform.position, Containers.Instance.LevelMapIcons);
+        mapIcon = mapIconToSpawn.Spawn(mapIconToSpawn.transform.position, Containers.Instance.MapIcons);
         mapIconToSpawn.enabled = false;
     }
 

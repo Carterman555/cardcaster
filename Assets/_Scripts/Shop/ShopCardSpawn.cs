9244ace8ec123d5c68240287bfca7f692aa26ebe
Carterman555 <25chart555@gmail.com>
2025-02-10 14:06:12 -0700

    5 rooms

---
+++ Assets/_Scripts/Shop/ShopCardSpawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopCardSpawn : MonoBehaviour {
+
+    [SerializeField] private ShopCard shopItemPrefab;
+
+    private void OnEnable() {
+        SpawnCard();
+    }
+
+    private void SpawnCard() {
+        ShopCard shopItem = shopItemPrefab.Spawn(transform.position, transform);
+
+        int currentLevel = GameSceneManager.Instance.GetLevel();
+        List<ScriptableCardBase> possibleCards = ResourceSystem.Instance.GetUnlockedCardsUpToLevel(currentLevel);
+        ScriptableCardBase randomCard = possibleCards.RandomItem();
+
+        shopItem.SetCard(randomCard);
+    }
+}

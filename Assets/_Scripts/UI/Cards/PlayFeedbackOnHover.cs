49cfbd5d061d41a0e77b4451a5d576c0a1ae94e5
Carterman555 <25chart555@gmail.com>
2025-02-26 09:21:57 -0700

    Try fix handcard movement, Before change hand cards to no revert

--- Assets/_Scripts/UI/Cards/PlayFeedbackOnHover.cs
+++ Assets/_Scripts/UI/Cards/PlayFeedbackOnHover.cs
@@ -1,4 +1,6 @@
 using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,8 +11,19 @@ public class PlayFeedbackOnHover : MonoBehaviour, IPointerEnterHandler, IPointer
     [ConditionalHide("playSFX")][SerializeField] private AudioClips OnEnterClips;
     [ConditionalHide("playSFX")][SerializeField] private AudioClips OnExitClips;
 
-    //... so component can be enabled and disabled
-    private void OnEnable() { }
+    private Queue<FeedbacksCommand> commands = new();
+
+    public enum FeedbacksCommand { PlayNormal, PlayReversed, RevertToNormal, RevertToReversed }
+
+    private HandCard card;
+
+    private void OnEnable() {
+        commands.Clear();
+
+        card = GetComponent<HandCard>();
+
+        StartCoroutine(ExecuteCommands());
+    }
 
     public void OnPointerEnter(PointerEventData eventData) {
 
@@ -18,12 +31,15 @@ public class PlayFeedbackOnHover : MonoBehaviour, IPointerEnterHandler, IPointer
             return;
         }
 
+        print($"{card.GetCard().CardType}: OnPointEnter");
+
         if (hoverFeedback.IsPlaying) {
-            hoverFeedback.Revert();
+            commands.Enqueue(FeedbacksCommand.RevertToNormal);
+            print($"{card.GetCard().CardType}: Queue Revert");
         }
         else {
-            hoverFeedback.SetDirectionTopToBottom();
-            hoverFeedback.PlayFeedbacks();
+            commands.Enqueue(FeedbacksCommand.PlayNormal);
+            print($"{card.GetCard().CardType}: Queue Play Normal");
         }
 
         if (playSFX) {
@@ -37,16 +53,79 @@ public class PlayFeedbackOnHover : MonoBehaviour, IPointerEnterHandler, IPointer
             return;
         }
 
+        print($"{card.GetCard().CardType}: OnPointExit");
+
+
         if (hoverFeedback.IsPlaying) {
-            hoverFeedback.Revert();
+            commands.Enqueue(FeedbacksCommand.RevertToReversed);
+            print($"{card.GetCard().CardType}: Queue Revert");
         }
         else {
-            hoverFeedback.SetDirectionBottomToTop();
-            hoverFeedback.PlayFeedbacks();
+            commands.Enqueue(FeedbacksCommand.PlayReversed);
+            print($"{card.GetCard().CardType}: Queue Play Reverse");
         }
 
         if (playSFX) {
             AudioManager.Instance.PlaySound(OnEnterClips);
         }
     }
+
+    private IEnumerator ExecuteCommands() {
+
+        while (enabled) {
+
+            //while (commands.Count > 2) {
+            //    print($"Overload dequed: {commands.Dequeue()}");
+
+            //}
+
+            if (commands.TryDequeue(out FeedbacksCommand command)) {
+
+                if (command == FeedbacksCommand.PlayNormal) {
+                    hoverFeedback.SetDirectionTopToBottom();
+                    hoverFeedback.PlayFeedbacks();
+
+                    print($"{card.GetCard().CardType}: Execute Play Normal");
+                }
+                else if (command == FeedbacksCommand.PlayReversed) {
+                    hoverFeedback.SetDirectionBottomToTop();
+                    hoverFeedback.PlayFeedbacks();
+
+                    print($"{card.GetCard().CardType}: Execute Play Reversed");
+                }
+                else if (command == FeedbacksCommand.RevertToNormal) {
+                    print($"{card.GetCard().CardType}: Execute Revert To Normal");
+
+                    if (hoverFeedback.IsPlaying) {
+                        hoverFeedback.Revert();
+                        print($"Reverted to {hoverFeedback.Direction}");
+                    }
+                    else {
+                        hoverFeedback.SetDirectionTopToBottom();
+                        hoverFeedback.PlayFeedbacks();
+                        print($"{card.GetCard().CardType}: Set direction to {hoverFeedback.Direction}");
+                    }
+                }
+                else if (command == FeedbacksCommand.RevertToReversed) {
+                    print($"{card.GetCard().CardType}: Execute Revert To Reversed");
+
+                    if (hoverFeedback.IsPlaying) {
+                        hoverFeedback.Revert();
+                        print($"{card.GetCard().CardType}: Reverted to {hoverFeedback.Direction}");
+                    }
+                    else {
+                        hoverFeedback.SetDirectionBottomToTop();
+                        hoverFeedback.PlayFeedbacks();
+                        print($"{card.GetCard().CardType}: Set direction to {hoverFeedback.Direction}");
+                    }
+                }
+
+                float executeCooldown = 0.1f;
+                yield return new WaitForSeconds(executeCooldown);
+            }
+
+            yield return null;
+        }
+    }
 }
+

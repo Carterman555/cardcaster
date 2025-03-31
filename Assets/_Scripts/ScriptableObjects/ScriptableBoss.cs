e19e481962dbff169d5e5133d91063eeb09aa9f3
Carterman555 <25chart555@gmail.com>
2025-03-31 10:02:27 -0600

    Localize key bindings, localize UI stuff

--- Assets/_Scripts/ScriptableObjects/ScriptableBoss.cs
+++ Assets/_Scripts/ScriptableObjects/ScriptableBoss.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 
 [CreateAssetMenu(fileName = "New Boss", menuName = "Unit/Boss")]
 public class ScriptableBoss : ScriptableObject {
 
-    [field: SerializeField] public string Name { get; private set; }
-    [field: SerializeField] public string Description { get; private set; }
+    [field: SerializeField] public LocalizedString LocName { get; private set; }
 
     [field: SerializeField] public GameObject Prefab { get; private set; }
 
--- Assets/_Scripts/SetStatTexts.cs
+++ Assets/_Scripts/SetStatTexts.cs
@@ -9,8 +9,15 @@ public class SetStatTexts : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI killsText;
     [SerializeField] private TextMeshProUGUI levelText;
 
+    // for localized string event
+    public int KillCount;
+    public int Level;
+
     private void OnEnable() {
         killsText.text = "Kills: " + GameStatsTracker.Instance.GetKills();
         levelText.text = "Level: " + GameSceneManager.Instance.GetLevel();
+
+        KillCount = GameStatsTracker.Instance.GetKills();
+        Level = GameSceneManager.Instance.GetLevel();
     }
 }

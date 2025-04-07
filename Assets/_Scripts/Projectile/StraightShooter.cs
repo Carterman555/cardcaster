bf6b5c0e2ddfb13724c06b7b2528b88b3042a35d
Carterman555 <25chart555@gmail.com>
2025-04-07 09:11:06 -0600

    Swing state shoots, start laser visual

---
+++ Assets/_Scripts/Projectile/StraightShooter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightShooter : MonoBehaviour {
+
+    [SerializeField] private StraightMovement projectilePrefab;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private Transform shootFromPoint;
+
+    [SerializeField] private float shootCooldown;
+    private float shootTimer;
+
+    [SerializeField] private bool useOverrideSpeed;
+    [ConditionalHide("useOverrideSpeed")][SerializeField] private float overrideSpeedValue;
+
+    [SerializeField] private float damage;
+    [SerializeField] private float knockbackStrength;
+
+    private void Update() {
+        shootTimer += Time.deltaTime;
+        if (shootTimer > shootCooldown) {
+            StraightMovement projectile = projectilePrefab.Spawn(projectileSpawnPoint.position, Containers.Instance.Projectiles);
+
+            Vector2 direction = (projectileSpawnPoint.position - shootFromPoint.position).normalized;
+
+            if (useOverrideSpeed) {
+                projectile.Setup(direction, overrideSpeedValue);
+            }
+            else {
+                projectile.Setup(direction);
+            }
+
+            projectile.GetComponent<DamageOnContact>().Setup(damage, knockbackStrength);
+
+            shootTimer = 0;
+        }
+    }
+}

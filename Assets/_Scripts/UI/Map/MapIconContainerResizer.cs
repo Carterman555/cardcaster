a2aa7c6e9f0d2e86174b860e55a693df3aa95812
Carterman555 <25chart555@gmail.com>
2025-04-22 10:21:37 -0600

    Finish map basics, start room teleport

--- Assets/_Scripts/UI/Map/MapIconContainerResizer.cs
+++ Assets/_Scripts/UI/Map/MapIconContainerResizer.cs
@@ -11,17 +11,20 @@ public class MapIconContainerResizer : MonoBehaviour {
     [SerializeField] private float padding;
 
     [ContextMenu("Resize")]
-    public void ResizeAndPosition(float scale = 1f) {
+    public void ResizeAndPosition() {
 
         rectTransform = GetComponent<RectTransform>();
-
-        float inverseScale = 1f / scale;
 
         Vector2 min = Vector2.one * float.MaxValue;
         Vector2 max = Vector2.one * float.MinValue;
 
         Image[] iconImages = GetComponentsInChildren<Image>().Where(i => i != GetComponent<Image>()).ToArray();
         foreach (Image image in iconImages) {
+
+            if (image.color.a == 0) {
+                continue;
+            }
+
             Vector3[] imageCorners = new Vector3[4];
             image.rectTransform.GetWorldCorners(imageCorners);
 
@@ -44,12 +47,12 @@ public class MapIconContainerResizer : MonoBehaviour {
         }
 
         
-        rectTransform.sizeDelta = new Vector2((max.x - min.x) + (padding * 2), (max.y - min.y) + (padding * 2)) * inverseScale;
+        rectTransform.sizeDelta = new Vector2((max.x - min.x) + (padding * 2), (max.y - min.y) + (padding * 2));
 
         Vector3[] containerCorners = new Vector3[4];
         rectTransform.GetWorldCorners(containerCorners);
 
-        Vector2 offset = new Vector2((containerCorners[0].x - min.x) + padding, (containerCorners[0].y - min.y) + padding) * inverseScale;
+        Vector2 offset = new Vector2((containerCorners[0].x - min.x) + padding, (containerCorners[0].y - min.y) + padding);
 
         foreach (Image iconImage in iconImages) {
             iconImage.rectTransform.anchoredPosition += offset;

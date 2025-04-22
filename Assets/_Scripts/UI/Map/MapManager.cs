469e1d9e9536c8376dc1b691519d71934993d68d
Carterman555 <25chart555@gmail.com>
2025-04-22 09:22:25 -0600

    Work on map (finish room icons)

--- Assets/_Scripts/UI/Map/MapManager.cs
+++ Assets/_Scripts/UI/Map/MapManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,8 @@ public class MapManager : MonoBehaviour {
     [SerializeField] private float mapScaleFactor;
     private const float WORLD_TO_MAP_SCALE = 100f;
 
+    [SerializeField] private MapIconContainerResizer mapIconContainerResizer;
+
     [Header("To Copy From Minimap")]
     [SerializeField] private Transform mapIconContainer;
     [SerializeField] private Transform mapOutlineIconContainer;
@@ -15,22 +18,43 @@ public class MapManager : MonoBehaviour {
     [SerializeField] private Transform miniMapIconContainer;
     [SerializeField] private Transform miniMapOutlineIconContainer;
 
+    private List<Image> spawnedImages = new();
+
     private void OnEnable() {
-        
-        foreach (Transform miniMapIcon in miniMapIconContainer) {
-            if (miniMapIcon.TryGetComponent(out Image miniMapImage)) {
-                Image mapImage = miniMapImage.Spawn(mapIconContainer);
-                mapImage.GetComponent<RectTransform>().anchoredPosition = miniMapImage.GetComponent<RectTransform>().anchoredPosition;
-            }
+        StartCoroutine(SetupMap());
+    }
+
+    private IEnumerator SetupMap() {
+        spawnedImages.Clear();
+
+        float miniMapToMapScaleFactor = mapScaleFactor / MinimapManager.Instance.MinimapScaleFactor;
+
+        Image[] iconImages = miniMapIconContainer.GetComponentsInChildren<Image>()
+            .Where(i => i != miniMapIconContainer.GetComponent<Image>()).ToArray();
+
+        foreach (Image miniMapImage in iconImages) {
+            Image mapImage = miniMapImage.Spawn(mapOutlineIconContainer);
+
+            RectTransform mapImageTransform = mapImage.GetComponent<RectTransform>();
+            RectTransform minimapImageTransform = miniMapImage.GetComponent<RectTransform>();
+
+            mapImageTransform.anchoredPosition = minimapImageTransform.anchoredPosition * miniMapToMapScaleFactor;
+            mapImageTransform.sizeDelta = minimapImageTransform.sizeDelta * miniMapToMapScaleFactor;
+
+            spawnedImages.Add(mapImage);
+
+            mapImage.Fade(1f); // debug 
         }
 
-        foreach (Transform miniMapOutlineIcon in miniMapOutlineIconContainer) {
-            if (miniMapOutlineIcon.TryGetComponent(out Image miniMapImage)) {
-                Image mapImage = miniMapImage.Spawn(mapOutlineIconContainer);
-                mapImage.GetComponent<RectTransform>().anchoredPosition = miniMapImage.GetComponent<RectTransform>().anchoredPosition;
-            }
-        }
+        mapIconContainerResizer.ResizeAndPosition(transform.localScale.x);
 
+        yield return new WaitForSeconds(1f);
+    }
+
+    private void OnDisable() {
+        foreach (Image mapImage in spawnedImages) {
+            mapImage.gameObject.ReturnToPool();
+        }
     }
 
     private Vector2 WorldToIconPos(Vector2 worldPos) {

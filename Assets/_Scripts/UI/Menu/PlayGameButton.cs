a6fd8dbb496cdef2fa61e80d18fb4cb1e78a5bb3
Carterman555 <25chart555@gmail.com>
2025-05-31 10:01:01 -0600

    Shared - polish, fix bugs
    can hold attack, don't force tutorial, fix tutorial not being able to open map

--- Assets/_Scripts/UI/Menu/PlayGameButton.cs
+++ Assets/_Scripts/UI/Menu/PlayGameButton.cs
@@ -2,27 +2,8 @@ using UnityEngine;
 
 public class PlayGameButton : GameButton {
 
-    [SerializeField] private bool alwaysTutorial;
-    [SerializeField] private bool neverTutorial;
-
     protected override void OnClick() {
         base.OnClick();
-
-        bool startTutorial;
-        if (alwaysTutorial) {
-            startTutorial = true;
-        }
-        else if (neverTutorial) {
-            startTutorial = false;
-        }
-        else {
-            startTutorial = !ES3.Load<bool>("TutorialCompleted", false);
-        }
-        
-        GameSceneManager.Instance.StartGame(startTutorial);
-
-        if (startTutorial) {
-            Tutorial.ResetPlayerDied();
-        }
+        GameSceneManager.Instance.StartGame();
     }
 }
